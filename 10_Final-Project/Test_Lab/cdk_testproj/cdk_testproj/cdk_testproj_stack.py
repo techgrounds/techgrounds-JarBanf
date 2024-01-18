b90cf1ad4ff87e3e588b9ce885a86d443a17ad10
Jared <jaredbanfield@hotmail.com>
2024-01-18 23:30:03 +0100

    update

--- 10_Final-Project/Test_Lab/cdk_testproj/cdk_testproj/cdk_testproj_stack.py
+++ 10_Final-Project/Test_Lab/cdk_testproj/cdk_testproj/cdk_testproj_stack.py
@@ -11,45 +11,74 @@ class CdkTestprojStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+########################################################################################################
+
         # create VPC with all default parameters.
         # when no parameters are assigned, it deploys only default values.
-        """
-        my_vpc = ec2.Vpc(self, 'MyTestVpc', 
+        # """
+        my_vpc = ec2.Vpc(self, 'MyTestVpc',
+            ip_addresses = ec2.IpAddresses.cidr('10.0.0.0/16'), # desired CIDR range
+            max_azs=0,
+            nat_gateways=0, 
         )
-        """
 
-        # Specify Availability Zones
+        subnet = ec2.CfnSubnet(self, 'AZa_Webserver',
+            vpc_id=my_vpc.vpc_id,
+            cidr_block='10.0.10.0/24',
+            availability_zone='eu-central-1a',
+        )
+        # """
+
+########################################################################################################
+        
+            # Availability zones this VPC spans
         availability_zones = [
             'eu-central-1a', 
-            # 'eu-central-1b'
+            'eu-central-1b',
+            # 'eu-central-1c',
         ]
 
-        # Specify Subnet Configuration
+            # 'subnetConfiguration' specifies the "subnet groups" to create.
+            # Every subnet group will have a subnet for each AZ, so this
+            # configuration will create `amount groups × amount AZs = total` subnets.
         subnet_configuration = [
             ec2.SubnetConfiguration(
-                subnet_type = ec2.SubnetType.PUBLIC, # Subnet connected to the internet
-                name = 'public 1',
-                cidr_mask = 28 # IP adressess 16 (14 usable)
+                    # 'subnetType' controls Internet access
+                subnet_type = ec2.SubnetType.PUBLIC,    # connected to the internet
+                
+                    # 'name' is used to name this particular subnet group. You will have to
+                    # use the name for subnet selection if you have more than one subnet
+                    # group of the same type.
+                name = 'Webserver',                     # subnet for the webserver and webserver database
+                
+                    #'cidrMask' specifies the IP addresses in the range of individual
+                    # subnets in the group.
+                cidr_mask = 28,                         # The subnets in this group will contain: IP adressess 16 (14 usable)
             ),
             ec2.SubnetConfiguration(
-                subnet_type = ec2.SubnetType.PUBLIC, # Subnet that routes to the internet, but not vice versa.
-                name = 'public 2',
-                cidr_mask = 28 # IP adressess 16 (14 usable)
+                subnet_type = ec2.SubnetType.PUBLIC,    # connected to the internet
+                name = 'Adminserver',                   # subnet for the Adminserver
+                cidr_mask = 28,                         # IP adressess 16 (14 usable)
             ),
             ec2.SubnetConfiguration(
-                subnet_type = ec2.SubnetType.PRIVATE_WITH_EGRESS, # Subnet that routes to the internet, but not vice versa.
-                name = 'private_egress 2',
-                cidr_mask = 26 # IP adressess 64 (62 usable)
+                subnet_type = ec2.SubnetType.PRIVATE_WITH_EGRESS,   # routes to the internet, but not vice versa.
+                name = 'Workstations',                              # subnet for the Workstations
+                cidr_mask = 26,                                     # IP adressess 64 (62 usable)
             )
         ]
 
-        #create VPC ...
-        # """
+            #create VPC
+        """
         my_vpc = ec2.Vpc(self, 'MyTestVpc',
-            availability_zones = availability_zones,
-            ip_addresses = ec2.IpAddresses.cidr('10.0.0.0/16'), # desired CIDR range
-            nat_gateways = 0, # Number of NAT Gateways
-            subnet_configuration = subnet_configuration
+            availability_zones = availability_zones,    # refers to configuration above
+            
+                # 'IpAddresses' configures the IP range and size of the entire VPC.
+            ip_addresses = ec2.IpAddresses.cidr('10.0.0.0/16'),
+
+                # The number of NAT Gateways to create.
+            nat_gateways = 0,
+            subnet_configuration = subnet_configuration # refers to configuration above
         )
-        # """
-        
+        """
+
+########################################################################################################
